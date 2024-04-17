e0a7767d3c86a2deeb79c04ad16d7c99149b5ad2
manjuhiremath <manjuhiremath1352@gmail.com>
2024-04-17 13:22:14 +0530

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,130 +1,78 @@
-import * as React from 'react';
+// import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
+// import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
+// import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
+// import Menu from '@mui/material/Menu';
+// import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 
 // import Button from '@mui/material/Button';
 
-import MenuItem from '@mui/material/MenuItem';
+// import MenuItem from '@mui/material/MenuItem';
 
 
-const pages = ['Home', 'About', 'Projects' , 'Contact'];
+// const pages = ['Home', 'About', 'Projects' , 'Contact'];
 
 
 function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  // const [anchorElNav, setAnchorElNav] = React.useState(null);
 
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
+  // const handleOpenNavMenu = (event) => {
+  //   setAnchorElNav(event.currentTarget);
+  // };
 
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+  // const handleCloseNavMenu = () => {
+  //   setAnchorElNav(null);
+  // };
 
 
 
   return (
     <AppBar position="static" sx={{ bgcolor: 'white' }}>
-      <Container maxWidth="xl" >
-        <Toolbar disableGutters>
+      {/* <Container maxWidth="xl" >
+      <Toolbar disableGutters sx={{ justifyContent: 'center' }}>
           
           <Typography
             variant="h6"
             noWrap
             component="a"
-            // href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily:"sans-serif",
-              fontWeight: 700,
-              letterSpacing: '.1rem',
-              color: 'black',
-              opacity:'70%',
-              textDecoration: 'none',
-            }}
+              sx={{
+                textAlign: 'center',
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'sans-serif',
+                fontWeight: 700,
+                letterSpacing: '.1rem',
+                color: 'black',
+                opacity: '70%',
+                textDecoration: 'none',
+              }}
           >
             Manju.dev
           </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="black"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem sx={{color:'black'}} key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center" color='black'>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} /> */}
           <Typography
             variant="h5"
             noWrap
             component="a"
-            // href="#app-bar-with-responsive-menu"
+          
             sx={{
               mr: 2,
               display: { xs: 'flex', md: 'none' },
               flexGrow: 1,
               fontFamily:"sans-serif",
               fontWeight: 700,
-              // letterSpacing: '.1rem',
               color: 'black',
               textDecoration: 'none',
             }}
           >
              Manju.dev
           </Typography>
-          {/* <Box sx={{ flexGrow: 2, display: { xs: 'none', md: 'flex'} }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2,color:'black', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box> */}
-
-          
         </Toolbar>
-      </Container>
+      </Container> */}
     </AppBar>
   );
 }
